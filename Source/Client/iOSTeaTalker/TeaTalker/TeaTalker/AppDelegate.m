0014aaa1e8704f61a93c14f20386b490bf7f953a
taojigu <user@localhost.(none)>
2014-02-08 17:28:58 +0800

    add Storyboard

--- Source/Client/iOSTeaTalker/TeaTalker/TeaTalker/AppDelegate.m
+++ Source/Client/iOSTeaTalker/TeaTalker/TeaTalker/AppDelegate.m
@@ -14,6 +14,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    return YES;
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
