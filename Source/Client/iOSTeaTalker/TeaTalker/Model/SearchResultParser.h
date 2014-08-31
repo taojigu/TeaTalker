0ed12a11013a5fbc857f59b76e8e9abe558059ae
VooleDev6 <taojigu@163.com>
2014-08-31 17:40:25 +0800

    add search result

---
+++ Source/Client/iOSTeaTalker/TeaTalker/Model/SearchResultParser.h
@@ -0,0 +1,13 @@
+//
+//  SearchResultParser.h
+//  TeaTalker
+//
+//  Created by VooleDev6 on 14-8-31.
+//  Copyright (c) 2014年 voole. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SearchResultParser : NSObject
+
+@end
---
+++ Source/Client/iOSTeaTalker/TeaTalker/Model/SearchResultParser.m
@@ -0,0 +1,13 @@
+//
+//  SearchResultParser.m
+//  TeaTalker
+//
+//  Created by VooleDev6 on 14-8-31.
+//  Copyright (c) 2014年 voole. All rights reserved.
+//
+
+#import "SearchResultParser.h"
+
+@implementation SearchResultParser
+
+@end
